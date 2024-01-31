8be160e6c5fa01a468154bf121bf4623d2e8251c
jose am talavera <115177988+joseamtalavera@users.noreply.github.com>
2024-01-31 18:57:42 +0200

    url showemails

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -70,7 +70,8 @@ function Register() {
     };
 
     const showEmails = async () => {
-        const response = await fetch('http://localhost:5003/api/emails');
+        //const response = await fetch('http://localhost:5003/api/emails');
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/api/register`);
         const data = await response.json();
         setEmails(data);
     };
